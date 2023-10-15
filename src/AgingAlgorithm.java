e2b0fde0a8ea2e6babbacb975e2bf7155d3e8e67
CristianCaro915 <c.caro@uniandes.edu.co>
2023-10-15 17:27:18 -0500

    aaa

--- src/AgingAlgorithm.java
+++ src/AgingAlgorithm.java
@@ -1,6 +1,20 @@
-
+import java.util.HashMap;
 //Thread encargado de ejecutar el algoritmo de envejecimiento cada 1 milisegundo.
 
 public class AgingAlgorithm {
-    
+    private int marcosPagina; //DATO COMPARTIDO ¿?
+    private HashMap<Integer, Integer> tablaPaginas; //DATO COMPARTIDO ¿?
+
+    public AgingAlgorithm(int marcosPagina)
+    {
+        this.marcosPagina=marcosPagina;
+        this.tablaPaginas= new HashMap<>();//tamaño¿?
+    }
+
+    //Hacer algoritmo
+    /*
+     * Asignar a cada página un contador
+     * Cada uno de los contadores se desplaza a la derecha 1 bit antes de agregar el bit R
+     * El bit R se agrega al bit de más a la izquierda
+     */
 }
