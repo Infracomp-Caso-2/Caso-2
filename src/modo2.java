f2156baa211f8817cad72340acf1577bf34581a2
Ana Sofía Padilla Daza <78044651+aspd-2501@users.noreply.github.com>
2023-10-16 21:38:14 -0500

    modo 2 perfeccionado, no esta completo UwU

--- src/modo2.java
+++ src/modo2.java
@@ -1,34 +1,73 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class modo2 {
+public class modo2 extends Thread {
+    private AgingAlgorithm agingAlgorithm;
+    private MarcoPagina marcoPagina;
 
-    private int marcos = 0;
-    private String archivo;
-    private Map<String, ArrayList<Integer>> referencias = new HashMap<String, ArrayList<Integer>>();
+    public static Integer contadorFallas = 0;
+    private ArrayList<Integer> referencias = new ArrayList<Integer>();
+    private String nombreArchivo;
+    private TablaPaginas tablaPaginas;
 
-    public modo2(int marcos, String archivo) {
-        this.marcos = marcos;
-        this.archivo = archivo;
+    public modo2(String nombreArchivo, AgingAlgorithm agingAlgorithm, MarcoPagina marcoPagina) {
+        this.nombreArchivo = nombreArchivo;
+        this.agingAlgorithm = agingAlgorithm;
+        this.marcoPagina = marcoPagina;
     }
 
-    public void cargarListaReferencias(marcos, archivo) {
-        // Cargar referencias
+    public void run() {
+        try {
+            File file = new File(nombreArchivo);
+            Scanner scanner = new Scanner(file);
 
-        for (int i = 0; i < marcos; i++) {
-            // Cargar marcos
-            ArrayList<Integer> marco = new ArrayList<Integer>();
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            marco.add(0);
-            referencias.put("Marco " + i, marco);
+            Integer contadorLineas = 0;
+
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine();
+                if (contadorLineas < 6) {
+                    contadorLineas++;
+                    continue;
+                } else if (contadorLineas == 6) {
+                    Integer totalPages = Integer.valueOf(line.split("=")[1]);
+                    tablaPaginas = new TablaPaginas(totalPages);
+                    contadorLineas++;
+                } else {
+                    Integer page = Integer.valueOf(line.split(",")[1]);
+                    leerReferencias(page);
+                    referencias.add(page);
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado");
+            e.printStackTrace();
         }
-    
     }
+
+    public void leerReferencias(Integer page) {
+        // Preguntar si la referencia esta en la TP
+        // Si esta...
+        // Si no esta entonces la referencia se mete en la TP y en el marco de paginas
+
+        Boolean estaEnTP = tablaPaginas.estaReferenciaEnTP(page);
+
+        if (estaEnTP) {
+            // Si esta en la TP entonces se actualiza el bit de referencia
+            // agingAlgorithm.actualizarBitReferencia(page);
+        } else {
+            // Si no esta en la TP entonces se mete en la TP y en el marco de paginas
+            contadorFallas++;
+            if (marcoPagina.tieneEspacio()) {
+                Integer nuevaPosicion = marcoPagina.agregarPagina(page);
+                tablaPaginas.agregarPagina(page, nuevaPosicion);
+            } else {
+            }
+        }
+
+    }
+
 }
